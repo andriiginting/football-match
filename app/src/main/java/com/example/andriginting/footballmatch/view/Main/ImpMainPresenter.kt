d40e729565b78c260b9d77bb6a19e9fbd4494d9c
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-15 01:37:55 +0700

    [andri] create presenter implementation class

---
+++ app/src/main/java/com/example/andriginting/footballmatch/view/Main/ImpMainPresenter.kt
@@ -0,0 +1,25 @@
+package com.example.andriginting.footballmatch.view.Main
+
+import android.content.Context
+import com.example.andriginting.footballmatch.R
+import com.example.andriginting.footballmatch.model.ClubModel
+import com.example.andriginting.footballmatch.presenter.MainContract
+
+class ImpMainPresenter(view: MainContract.View,
+                       private val context: Context,
+                       private val list: ArrayList<ClubModel>) : MainContract.Presenter {
+
+    override fun addClub(): ArrayList<ClubModel> {
+        val icon = context.resources.obtainTypedArray(R.array.club_logo)
+        val name = context.resources.getStringArray(R.array.club_name)
+        val clubDescription = context.resources.getStringArray(R.array.club_description)
+        for (items in name.indices) {
+            list.add(ClubModel(name[items],
+                    icon.getResourceId(items, -1),
+                    clubDescription[items]))
+        }
+        icon.recycle()
+        return list
+    }
+
+}
