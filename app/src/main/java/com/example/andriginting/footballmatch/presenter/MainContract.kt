23ae74767c40c0d43115bab541b955e2a2d83023
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-15 01:37:31 +0700

    [andri] modified addClub func

--- app/src/main/java/com/example/andriginting/footballmatch/presenter/MainContract.kt
+++ app/src/main/java/com/example/andriginting/footballmatch/presenter/MainContract.kt
@@ -1,11 +1,13 @@
 package com.example.andriginting.footballmatch.presenter
 
+import com.example.andriginting.footballmatch.model.ClubModel
+
 interface MainContract {
     interface View{
         fun initComponent()
     }
 
     interface Presenter{
-        fun addClub()
+        fun addClub(): List<ClubModel>
     }
 }
