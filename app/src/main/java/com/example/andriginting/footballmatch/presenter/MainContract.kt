f6057e5fdebc4219826e0c14b171917065e27e90
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-15 20:54:05 +0700

    [andri] add loading indicator in main contract

--- app/src/main/java/com/example/andriginting/footballmatch/presenter/MainContract.kt
+++ app/src/main/java/com/example/andriginting/footballmatch/presenter/MainContract.kt
@@ -5,6 +5,8 @@ import com.example.andriginting.footballmatch.model.ClubModel
 interface MainContract {
     interface View{
         fun initComponent()
+        fun showLoadingIndicator()
+        fun hideLoadingIndicator()
     }
 
     interface Presenter{
