2c102418003085c177dd3bbb408378f40ccdfde2
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-15 20:50:46 +0700

    [andri] add detail activity

---
+++ app/src/main/java/com/example/andriginting/footballmatch/view/detail/DetailActivity.kt
@@ -0,0 +1,47 @@
+package com.example.andriginting.footballmatch.view.detail
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.bumptech.glide.Glide
+import com.example.andriginting.footballmatch.R
+import com.example.andriginting.footballmatch.model.ClubModel
+import com.example.andriginting.footballmatch.presenter.DetailContract
+
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
+
+
+class DetailActivity : AppCompatActivity(), DetailContract.View {
+
+    companion object {
+        const val DETAIL_CLUB = "club"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val data = intent.getParcelableExtra<ClubModel>(DETAIL_CLUB)
+        setToolbarTitle(data.clubName)
+
+        Glide.with(this).
+                load(data.clubLogo).
+                into(detail_club_logo)
+        detail_description_club.text = data.clubDetail
+    }
+
+    override fun setToolbarTitle(title: String) {
+        supportActionBar?.title = title
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> {onBackPressed()}
+        }
+        return true
+    }
+
+}
