a9db4addfa51eb9fa143203818c907e8f22b63fe
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-29 23:20:44 +0700

    [andri] create new team fragment

---
+++ app/src/main/java/com/example/andriginting/footballmatch/view/teams/TeamFragment.kt
@@ -0,0 +1,22 @@
+package com.example.andriginting.footballmatch.view.teams
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.andriginting.footballmatch.R
+
+class TeamFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_team, container, false)
+        return view
+    }
+
+
+}
